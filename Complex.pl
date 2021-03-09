45e584399fce81cbc1b9ec1c9ca1e380c5b9ebbf
Jay Malanker <jjmalankar@gmail.com>
2021-03-10 01:38:42 +0530

    Update Complex.pl

--- Complex.pl
+++ Complex.pl
@@ -1,6 +1,6 @@
 customer(tim, smith, 250.459).
 customer(tom, smith, 150.254).
 get_det(FirstN, LastN) :-
-    customer(FirstN, _, Due),
+    customer(FirstN, LastN, Due),
     % 2f shows upto 2 dec places
     format('~w ~s has $~2f due', [FirstN, LastN, Due]).
