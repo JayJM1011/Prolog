35796ef5873afa30026fa32a347bb1be69c376dc
Jay Malanker <jjmalankar@gmail.com>
2021-04-03 01:57:10 +0530

    Update Recursion.pl

--- Recursion.pl
+++ Recursion.pl
@@ -5,3 +5,10 @@ related(X, Y):-
 related(X, Y):-
     parent(X, Z); parent(Z, X),
     related(Z, Y).
+
+count_till_10(10):-
+    write(10).
+count_till_10(Other):-
+    write(Other), nl,
+    X is Other+ 1,
+    count_till_10(X).
